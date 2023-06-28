4b595a7dcb5a8ff59f02fa85644dee989011fc2c
abdallah-hatem <ahkortam@gmail.com>
2023-06-28 20:14:26 +0300

    swap apiBaseUrl to production

--- Services/Config.js
+++ Services/Config.js
@@ -1,2 +1,2 @@
-export const ApiBaseUrl = "http://localhost:3000/api/";
-export const ApiBaseUrl2 = "https://next-cyber-postgresql.vercel.app/";
+export const ApiBaseUrl2 = "http://localhost:3000/api/";
+export const ApiBaseUrl = "https://next-cyber-postgresql.vercel.app/";
